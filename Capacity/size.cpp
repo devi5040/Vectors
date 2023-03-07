56aa41cd7868b3fdfc7bd609827829b6ee183fa2
devi5040 <dpraidola@gmail.com>
2023-03-07 16:56:28 +0530

    size method for a vector

---
+++ Capacity/size.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> vect = {1, 2, 3, 4, 5, 6};
+    int n = vect.size();
+    cout << "The size of the vector is:" << endl;
+    cout << n << endl;
+    return 0;
+}
+
+/*
+    Output:
+    The size of the vector is:
+    6
+*/
